7b19435a84614c01b55485942d762c10a0d84388
hjlllll <44728983+hjlllll@users.noreply.github.com>
2023-12-21 15:17:10 +0800

    Add files via upload

---
+++ Annotation/dexterous_hands/utils_hand/rot6d.py
@@ -0,0 +1,110 @@
+import torch
+import numpy as np
+import transforms3d
+
+
+def get_rot6d_from_rot3d(rot3d):
+    global_rotation = np.array(transforms3d.euler.euler2mat(rot3d[0], rot3d[1], rot3d[2]))
+    return global_rotation.T.reshape(9)[:6]
+
+
+def compute_rotation_matrix_from_ortho6d(poses):
+    """
+    Code from
+    https://github.com/papagina/RotationContinuity
+    On the Continuity of Rotation Representations in Neural Networks
+    Zhou et al. CVPR19
+    https://zhouyisjtu.github.io/project_rotation/rotation.html
+    """
+    x_raw = poses[:, 0:3]  # batch*3
+    y_raw = poses[:, 3:6]  # batch*3
+        
+    x = normalize_vector(x_raw)  # batch*3
+    z = cross_product(x, y_raw)  # batch*3
+    z = normalize_vector(z)  # batch*3
+    y = cross_product(z, x)  # batch*3
+        
+    x = x.view(-1, 3, 1)
+    y = y.view(-1, 3, 1)
+    z = z.view(-1, 3, 1)
+    matrix = torch.cat((x, y, z), 2)  # batch*3*3
+    return matrix
+
+
+def robust_compute_rotation_matrix_from_ortho6d(poses):
+    """
+    Instead of making 2nd vector orthogonal to first
+    create a base that takes into account the two predicted
+    directions equally
+    """
+    x_raw = poses[:, 0:3]  # batch*3
+    y_raw = poses[:, 3:6]  # batch*3
+
+    x = normalize_vector(x_raw)  # batch*3
+    y = normalize_vector(y_raw)  # batch*3
+    middle = normalize_vector(x + y)
+    orthmid = normalize_vector(x - y)
+    x = normalize_vector(middle + orthmid)
+    y = normalize_vector(middle - orthmid)
+    # Their scalar product should be small !
+    # assert torch.einsum("ij,ij->i", [x, y]).abs().max() < 0.00001
+    z = normalize_vector(cross_product(x, y))
+
+    x = x.view(-1, 3, 1)
+    y = y.view(-1, 3, 1)
+    z = z.view(-1, 3, 1)
+    matrix = torch.cat((x, y, z), 2)  # batch*3*3
+    # Check for reflection in matrix ! If found, flip last vector TODO
+    # assert (torch.stack([torch.det(mat) for mat in matrix ])< 0).sum() == 0
+    return matrix
+
+
+def normalize_vector(v):
+    batch = v.shape[0]
+    v_mag = torch.sqrt(v.pow(2).sum(1))  # batch
+    v_mag = torch.max(v_mag, v.new([1e-8]))
+    v_mag = v_mag.view(batch, 1).expand(batch, v.shape[1])
+    v = v/v_mag
+    return v
+
+
+def cross_product(u, v):
+    batch = u.shape[0]
+    i = u[:, 1] * v[:, 2] - u[:, 2] * v[:, 1]
+    j = u[:, 2] * v[:, 0] - u[:, 0] * v[:, 2]
+    k = u[:, 0] * v[:, 1] - u[:, 1] * v[:, 0]
+        
+    out = torch.cat((i.view(batch, 1), j.view(batch, 1), k.view(batch, 1)), 1)
+        
+    return out
+
+#quaternion batch*4
+def compute_rotation_matrix_from_quaternion(quaternion):
+    batch=quaternion.shape[0]
+    
+    
+    quat = normalize_vector(quaternion).contiguous()
+    
+    qw = quat[...,0].contiguous().view(batch, 1)
+    qx = quat[...,1].contiguous().view(batch, 1)
+    qy = quat[...,2].contiguous().view(batch, 1)
+    qz = quat[...,3].contiguous().view(batch, 1)
+
+    # Unit quaternion rotation matrices computatation  
+    xx = qx*qx
+    yy = qy*qy
+    zz = qz*qz
+    xy = qx*qy
+    xz = qx*qz
+    yz = qy*qz
+    xw = qx*qw
+    yw = qy*qw
+    zw = qz*qw
+    
+    row0 = torch.cat((1-2*yy-2*zz, 2*xy - 2*zw, 2*xz + 2*yw), 1) #batch*3
+    row1 = torch.cat((2*xy+ 2*zw,  1-2*xx-2*zz, 2*yz-2*xw  ), 1) #batch*3
+    row2 = torch.cat((2*xz-2*yw,   2*yz+2*xw,   1-2*xx-2*yy), 1) #batch*3
+    
+    matrix = torch.cat((row0.view(batch, 1, 3), row1.view(batch,1,3), row2.view(batch,1,3)),1) #batch*3*3
+    
+    return matrix
